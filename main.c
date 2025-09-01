c177575867b9fe4d2a925707e238e4e161511347
ihawp <ihawpebay@gmail.com>
2025-09-01 11:56:24 -0700

    function pointers

--- main.c
+++ main.c
@@ -4,47 +4,45 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <math.h>
 
 struct Flags {
 	char filename[50];
 	char sunsetname[50];
 	uint32_t awesome;
+	void (*functionpointer)(char*);
 };
 
-int minusF(char *filename);
+void minusF(char *filename);
 size_t read_bytes(FILE *file, unsigned char *buffer, size_t bufsize);
 
-// DO SOME FILE OPERATIONS.
-// Pass the filename
-int minusF(char *filename) {
+/*
+*
+*
+*	minusF
+*
+*
+*
+*
+*/
+
+void minusF(char *filename) {
 
 	FILE *file;
-
-	printf("%s\n", filename);
-
 	file = fopen(filename, "r");
 
 	if (file == NULL) {
 		printf("Unable to open file.\n");
-		return 1;
+		return;
 	}
 
-	// char max size is 1024 bytes.
 	// Safer to call unsigned char because gcc compiles char as signed char, ARM makes it unsigned.
 	unsigned char buffer[16];
 	unsigned char *bufferpointer2 = malloc(2000);
 	unsigned char *bufferpointer = buffer;
 
-	// get the total bytes read from the file and print the file size
 	size_t bytesRead = read_bytes(file, bufferpointer, 16);
-
-	fseek(file, 0, SEEK_SET);
-
-	// another one
-	size_t bytesRead2 = read_bytes(file, bufferpointer2, 2000);
-
-//	printf("\nFile Size: %zu\n", bytesRead);
-//	printf("\nFile Size 2: %zu\n", bytesRead2);
+	printf("\nFile Size: %zu\n", bytesRead);
 
 	struct _stat st;
 	if (_stat("awesome.txt", &st) == 0) {
@@ -54,16 +52,21 @@ int minusF(char *filename) {
 	}
 
 	fclose(file);
-
-	return 1;
-
 }
+
+/*
+*
+*
+*	read_bytes
+*
+*
+*
+*
+*/
 
 size_t read_bytes(FILE *file, unsigned char *buffer, size_t bufsize) {
 
 	size_t total = 0;
-
-	printf("start of read_bytes");
 
 	size_t bytesRead;
 
@@ -75,48 +78,37 @@ size_t read_bytes(FILE *file, unsigned char *buffer, size_t bufsize) {
 		}
 	}
 
-	printf("end of read_bytes");
-
 	return total;
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-	Flags:
-	-------------
-	-f: open a file and print the result *twice?
+*
+*
+*	read_bytes
+*
+*
+*
+*
 */
 
-void parseArgs(int argc, char **argv, struct Flags *fmuse) {
+void execute() {
+	printf("executing");
+}
+
+/*
+*	Flags:
+*	-------------
+*	-f: open a file and print the result *twice?
+*
+*
+*	parseArgs
+*
+*
+*
+*
+*/
+
+void parse(int argc, char **argv, struct Flags *fmuse) {
 
 	// Can also do it like this so that it is allocated on the heap.
 	// struct FileManager *fmusepointer = malloc(sizeof(struct FileManager));
@@ -125,31 +117,39 @@ void parseArgs(int argc, char **argv, struct Flags *fmuse) {
 
 	for (int i = 0; i < argc; i++) {
 
-		if (strcmp(argv[i], "-a") == 0) {
-
-
-		} else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+		if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
 
 			char filename[50];
 			strncpy(filename, argv[i + 1], sizeof(filename) - 1);
 			filename[sizeof(filename) - 1] = '\0';
 			char *filenamepointer = filename;
-			minusF(filenamepointer);
 
-		} else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+			fmuse->functionpointer = minusF;
 
+			fmuse->functionpointer(filename);
 
 		}
 	}
 }
 
+/*
+*
+*
+*	main
+*
+*
+*
+*
+*/
+
 int main(int argc, char **argv) {
 
-	// Initialize the struct to track the submitted flags
+	// Initialize the struct to track the submitted flags.
 	struct Flags fmuse;
 	struct Flags *fmusepointer = &fmuse;
 
-	parseArgs(argc, argv, fmusepointer);
+	// Parse the arguments passed in argv.
+	parse(argc, argv, fmusepointer);
 
 	return 0;
 }
